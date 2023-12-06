5aaa1d1d70bdc8e5137f583ceacdae614c196161
Daniel Reichenpfader <daniel.reichenpfader@bfh.ch>
2023-12-06 14:19:50 +0100

    :truck: Move inference code acc. to DevOps concept

--- src/refinement_model/inference/service.py
+++ src/refinement_model/inference/service.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torch
 
 import constants
-import src.shared.model_helpers as helper
+import shared.model_helpers as helper
 
 model_ref = bentoml.models.get(constants.FACT_EXTRACTION_MODEL_NAME + ":latest")
 
@@ -134,11 +134,11 @@ class FactExtractionRunnable(bentoml.Runnable):
         structured_anchors_dataframe = pd.DataFrame(
             [predicted_anchor_tokens, predicted_anchor_tags], index=["tokens_anchors", "predicted_anchor_tags"]
         )
-        print("Anchors_dataframe: ", structured_anchors_dataframe)
+        #print("Anchors_dataframe: ", structured_anchors_dataframe)
 
         # Multi.label inference for facts:
         raw_fact_predictions = self.get_offsets_and_predicted_tags(example, self.tokenizer, outputs)
-        print("Raw fact predictions: ", raw_fact_predictions)
+        #print("Raw fact predictions: ", raw_fact_predictions)
         extracted_fact_predictions = []
         extracted_fact_tokens = []
         for pred in raw_fact_predictions:
@@ -167,4 +167,4 @@ if __name__ == "__main__":
     example = "MAMMOGRAFIE BEIDSEITS IN ZWEI EBENEN VOM 22.06.2021&#10;&#10;Fragestellung/Indikation&#10;Met. Adeno-Ca, unkl. Primarius. (CT-Befund vom 10.6.21 mit Vd. a. i.e.L. DD HCC, DD CCC.&#10;Positive Familienanamnese für Brustkrebs (Tante väterlicherseits).&#10;Malignität/Auffälligkeiten?&#10;&#10;Klinische Untersuchung und Ultraschall:&#10;Durchführung in der gynäkologischen Senologie dieser Klinik (Bericht siehe dort).&#10;&#10;Befund&#10;Zum Vergleich liegt die Voruntersuchung vom 28.09.2020 vor.&#10;&#10;Mammografie beidseits MLO und CC:&#10;Kutis und Subkutis unauffällig.&#10;Mittelfleckiges, teilweise involutiertes Drüsenparenchym beidseits.&#10;Kein malignomsuspekter Herdbefund. Keine suspekte Mikrokalkgruppe.&#10;&#10;Beurteilung&#10;Mammographisch kein Anhalt für Malignität beidseits.&#10;&#10;ACR-Typ b beidseits.&#10;BIRADS 1 beidseits.&#10;&#10;Falls auch der Ultraschallbefund der Brust unauffällig sein sollte, wäre eine weitere Befundabsicherung mittels MR Mammographie zu erwägen."
     runnable = FactExtractionRunnable()
     result = runnable.predict_facts_and_anchors(example)
-    print("Result :", result)
+    print(result)
